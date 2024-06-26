0e79f67dce744a7458d0d47c06fbe375b1ea88b9
unknown <santiagobavaresco@gmail.com>
2024-06-26 09:47:46 -0300

    reajuste logo facebook footer

--- app/(all)/footer.tsx
+++ app/(all)/footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
 				<path d="M16.375 1.66675H12.25C10.4266 1.66675 8.67789 2.36912 7.38869 3.61937C6.09932 4.86961 5.375 6.56531 5.375 8.33341V12.3334H1.25V17.6667H5.375V28.3334H10.875V17.6667H15L16.375 12.3334H10.875V8.33341C10.875 7.97979 11.0199 7.64065 11.2777 7.3906C11.5356 7.14056 11.8854 7.00008 12.25 7.00008H16.375V1.66675Z" stroke="#EF771C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 				</svg> */}
 				<div  className="text-orange-600 hover:text-blue-500 text-xl" >
-				<svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<svg width="20" height="20" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<rect x="0.75" y="0.75" width="19.5" height="19.5" rx="5.25" stroke="#EF771C" stroke-width="1.5"/>
 				<path d="M14 4H12.3636C11.6403 4 10.9466 4.31607 10.4352 4.87868C9.9237 5.44129 9.63636 6.20435 9.63636 7V8.8H8V11.2H9.63636V16H11.8182V11.2H13.4545L14 8.8H11.8182V7C11.8182 6.84087 11.8757 6.68826 11.9779 6.57573C12.0802 6.46322 12.219 6.4 12.3636 6.4H14V4Z" stroke="#EF771C" stroke-linecap="round" stroke-linejoin="round"/>
 				</svg>
