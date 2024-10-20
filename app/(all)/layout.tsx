1fe8580b40bd0a3b797e632b2d35a51debda3f6c
Santiago Bavaresco <santiagobavaresco@gmail.com>
2024-10-20 01:13:40 -0300

    clarify y analitics agregado

--- app/(all)/layout.tsx
+++ app/(all)/layout.tsx
@@ -21,6 +21,8 @@ import { usePathname } from 'next/navigation'
 
 import clsx from "clsx";
 import Footer from "../../components/footer";
+import Script from "next/script";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 
 
@@ -74,10 +76,23 @@ export default function RootLayout({
                     </section>} */}
                     {/* <Navbar />  */}
 
-
                     <div className="relative flex flex-col h-screen">
                         <main className="container mx-auto max-w-full">
                             {children}
+
+
+                            <Script id="clarity-script" type="text/javascript" strategy="afterInteractive">
+                            {`
+                                (function(c,l,a,r,i,t,y){
+                                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                                })(window, document, "clarity", "script", "ojjh5gdoug");
+                            `}
+                            </Script>
+
+                            <GoogleAnalytics gaId="G-X8EBB5PW7Y" />
+                            
                         </main>
                         
                        <Footer/>
